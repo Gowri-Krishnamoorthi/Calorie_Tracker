7a44f98c7567c9c524e5d5295a5431e196835e14
Gowri-Krishnamoorthi <Gowrikrishnamoorthi14@gmail.com>
2025-03-25 22:40:01 +0530

    calorie table calculation completed

---
+++ myapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Food , Consume
+# Create your views here.
+
+def index(request):
+    
+    if request.method == 'POST':
+       food_consumed = request.POST['food_consumed'] #namefood
+       consumed = Food.objects.get(name=food_consumed)  #food object
+       user = request.user
+
+       consume = Consume(user=user, food_consumed=consumed)
+       consume.save()
+
+       foods = Food.objects.all()
+
+    
+    # Fetch consumed foods for the current user
+    consumed_foods = Consume.objects.filter(user=request.user)
+    foods = Food.objects.all()
+
+    return render(request, 'myapp/index.html', {'foods': foods, 'consumed_foods': consumed_foods})
